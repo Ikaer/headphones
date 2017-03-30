77d9cf53943c7a7255473d1077a9aadb11223a96
Ikaer <lefebvrex627@gmail.com>
2017-03-30 13:39:09 +0200

    Update qbittorrent.py

--- headphones/qbittorrent.py
+++ headphones/qbittorrent.py
@@ -183,12 +183,12 @@ def addTorrent(link):
 
     qbclient = qbittorrentclient()
 
-		args = {'urls': link}
+    args = {'urls': link}
 
     logger.debug('headphones.CONFIG.QBITTORRENT_HANDLE_SAVEPATH(%s)' % headphones.CONFIG.QBITTORRENT_HANDLE_SAVEPATH)
 
     if headphones.CONFIG.QBITTORRENT_HANDLE_SAVEPATH:
-	args['savepath'] = headphones.CONFIG.DOWNLOAD_TORRENT_DIR
+        args['savepath'] = headphones.CONFIG.DOWNLOAD_TORRENT_DIR
 
     if headphones.CONFIG.QBITTORRENT_LABEL:
         args['category'] = headphones.CONFIG.QBITTORRENT_LABEL
